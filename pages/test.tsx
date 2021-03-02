8c245bf7372421f84d8df83c02f599378bed0a33
showone7 <li.andr1771@gmail.com>
2021-03-02 12:00:31 +0300

    f

---
+++ pages/test.tsx
@@ -0,0 +1,10 @@
+import Layout from '../components/layout';
+type TProps = {};
+
+export default function Homepage(props: TProps) {
+    return (
+        <Layout title="Homepage">
+            <p>Lorem Ipsum.....</p>
+        </Layout>
+    );
+}
