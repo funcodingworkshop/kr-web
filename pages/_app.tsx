285468a3d7ad0e2f949e467a701eeebad7dc6851
mmx369 <maksim@klishevich.com>
2021-07-01 22:52:44 +0300

    rrr

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -26,7 +26,6 @@ export default function App({ Component, pageProps }: AppProps) {
                 <SessionProvider session={pageProps.session}>
                     <CssBaseline />
                     <Component {...pageProps} />
-                    
                 </SessionProvider>
             </Provider>
         </ThemeProvider>
